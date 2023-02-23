5dccba2b13d19bd02c192da7d571cd1b0cda3bcc
Courtney Roth <123835714+Courtney-Roth@users.noreply.github.com>
2023-02-22 18:15:33 -0800

    streamlined random computer selector and game play

--- rps.js
+++ rps.js
@@ -1,14 +1,52 @@
-/* Random Computer Choice Generator */
-function getComputerChoice() {
-    const a = Math.floor(Math.random() * 3)
-    if (a == 0) {
-        return "Rock" 
-    } 
-    else if (a == 1) {
-        return "Scissors"
+// Set up initial scores for both players
+let playerScore = 0;
+let computerScore = 0;
+
+// Set up the game function to be called
+function playGame() {
+  // Loop until one player reaches 5 points
+  while (playerScore < 5 && computerScore < 5) {
+    // Ask the player to choose rock, paper, or scissors
+    let playerSelection = prompt("Choose rock, paper, or scissors");
+
+    // Make the player's selection lowercase
+    playerSelection = playerSelection.toLowerCase();
+
+    // Generate the computer's random selection
+    let computerSelection = Math.random();
+
+    // Determine the computer's choice based on the random number
+    if (computerSelection < 0.34) {
+      computerSelection = "rock";
+    } else if (computerSelection <= 0.67) {
+      computerSelection = "paper";
+    } else {
+      computerSelection = "scissors";
     }
-    else if (a == 2) {
-        return "Paper"  
+
+    // Compare the player's selection with the computer's selection
+    if (playerSelection === computerSelection) {
+      alert("It's a tie!");
+    } else if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+      playerScore++;
+      alert(`You win this round! Your score: ${playerScore} Computer score: ${computerScore}`);
+    } else {
+      computerScore++;
+      alert(`Computer wins this round! Your score: ${playerScore} Computer score: ${computerScore}`);
     }
+  }
+
+  // Determine the winner of the game based on the scores
+  if (playerScore > computerScore) {
+    alert("Congratulations! You win the game!");
+  } else {
+    alert("Sorry, you lost the game. Better luck next time!");
+  }
 }
 
+// Call the game function to start playing
+playGame();
